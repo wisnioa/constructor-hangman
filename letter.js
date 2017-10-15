1bb30df130f3e7f448ec359b640c2a91a7250b79
unknown <amanda.wisniowski@gmail.com>
2017-10-15 18:19:39 -0500

    adding pseudocode

--- letter.js
+++ letter.js
@@ -1,5 +1,20 @@
 
-function hideLetter(){
+var newWord = require('./word.js');
+
+var word = "";
+
+function letterConstructor(letter){
+for (i=0; i < newWord.length, i++;){
+
+this.character = letter;
+this.appear = false;
+
+
+
+
+
+console.log(word);
 
     
-}
+
+
--- word.js
+++ word.js
@@ -1,9 +1,14 @@
-//Randomizing words from word bank
 
+
+//Randomizing words from word bank
+var letterFile = require('./letter.js');
 var wordBank = ["margarita", "beer", "hurricane", "screwdriver", "martini", "wine"];
 
-var randomWord = wordBank[Math.floor(Math.random() * wordBank.length++)];
+var randomWord = Math.floor(Math.random() * wordBank.length);
+
+var newWord = wordBank[randomWord];
 
 
 
 
+module.exports = newWord; 
